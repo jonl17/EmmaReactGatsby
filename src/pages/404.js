e796d28e3f375fb09948ed632105fc9412df5775
Jón <Lorange>
2019-06-27 16:42:01 +0000

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,28 @@
 import React from "react"
+import { GlobalStyles } from "../components/GlobalStyles/index.js"
+import Wrap from "../components/Wrap"
+import Header from "../components/Header"
+import { graphql } from "gatsby"
 
-export default () => {
+export default ({ data }) => {
   return (
-    <div>
-      <p>This page was not found... What are you doing !?!?!?</p>
-    </div>
+    <Wrap>
+      <GlobalStyles></GlobalStyles>
+      <Header metadata={data.site.siteMetadata}></Header>
+      <div>
+        <p>This page was not found... What are you doing !?!?!?</p>
+      </div>
+    </Wrap>
   )
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        menuItems
+      }
+    }
+  }
+`
